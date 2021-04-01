d540883b47c454d59f5df9bf2f986c5213a45a49
Aleksandr Holoven <holoven.aleksandr@gmail.com>
2021-04-01 22:15:58 +0300

    fix db

--- src/migrations/down/2.CategoryTable.sql
+++ src/migrations/down/2.CategoryTable.sql
@@ -1,7 +1,5 @@
 START TRANSACTION;
-  ALTER TABLE "user" DROP CONSTRAINT  fk_category;
+  ALTER TABLE "user" DROP CONSTRAINT fk_category;
   ALTER TABLE "user" DROP categoryId;
-
   DROP TABLE category
 COMMIT;
-
--- src/migrations/up/1.UserTable.sql
+++ src/migrations/up/1.UserTable.sql
@@ -3,6 +3,5 @@ START TRANSACTION;
       id serial PRIMARY KEY,
       fname varchar NOT NULL,
       lname varchar NOT NULL,
-      isActive boolean NOT NULL
   );
 COMMIT;
