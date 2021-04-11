552838503db0a68dafc36ad4341737d1055dbe06
Aleksandr Holoven <holoven.aleksandr@gmail.com>
2021-04-11 22:03:31 +0300

    upd joi

--- src/validator.js
+++ src/validator.js
@@ -3,9 +3,8 @@ const Joi = require('joi');
 const schema = Joi.object({
   fname: Joi.string().min(5).required(),
   lname: Joi.string().min(5).required(),
-  // uname: Joi.string().min(5).required(),
-  // email: Joi.string().min(5).required(),
-  // active: Joi.bool().required(),
+  uname: Joi.string().min(5).required(),
+  email: Joi.string().min(5).required(),
 });
 
 module.exports = {
